c3013dff119084b0f25533fbc7bdf69c7791bbd3
Nikita Dutta <nikita.june26@gmail.com>
2023-10-02 22:11:23 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,13 @@
+// const heading =   React.createElement('h1',{id:'heading', class:'heading'}, 'Hello World from React');
+//  const root = ReactDOM.createRoot(document.getElementById('root'));
+//  root.render(heading);
+const parent =   React.createElement('div',{id:'parent', class:'parent'},
+ [React.createElement('div',{id:'child'},
+ [React.createElement('h1',{id:'heading'},'i am heading'),
+ React.createElement('h2',{id:'heading2'}, 'i am h2 tag')]),
+ 
+ React.createElement('div',{id:'child2'},
+ [React.createElement('h1',{id:'heading'},'i am heading'),
+ React.createElement('h2',{id:'heading2'}, 'i am h2 tag')])]);
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(parent);
