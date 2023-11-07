f1630faf0fc4033b317d1d15eecf412fbf458948
Nikita Dutta <nikita.june26@gmail.com>
2023-11-07 21:55:51 +0530

    episode03

--- app.js
+++ app.js
@@ -4,14 +4,40 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
+// 2nd assingment
 
-const parent =   React.createElement('div',{id:'parent', class:'parent'},
- [React.createElement('div',{id:'child'},
- [React.createElement('h1',{id:'heading'},'i am new heading'),
- React.createElement('h2',{id:'heading2'}, 'i am h2 tag')]),
+// const parent =   React.createElement('div',{id:'parent', class:'parent'},
+//  [React.createElement('div',{id:'child'},
+//  [React.createElement('h1',{id:'heading'},'i am new h1 heading'),
+//  React.createElement('h2',{id:'heading2'}, 'i am h2 tag')]),
  
- React.createElement('div',{id:'child2'},
- [React.createElement('h1',{id:'heading'},'i am heading'),
- React.createElement('h2',{id:'heading2'}, 'i am h2 tag')])]);
+//  React.createElement('div',{id:'child2'},
+//  [React.createElement('h1',{id:'heading'},'i am heading'),
+//  React.createElement('h2',{id:'heading2'}, 'i am h2 tag')])]);
+
+// 3rd assingment
+
+// const Heading = () =>(
+// <h1 id="heading" className="head"  tabIndex={1}>Heading using JSX</h1>);
+
+// React Functional Component
+// const num = 10;
+
+// const elem = <span>span text</span>
+const title = () =>  (
+    <h1 className ="head" tabIndex="5">
+        
+ heading    </h1>
+)
+const Headingfunctional = () =>(
+
+    <div id="container">  
+   {title()}
+    {/* <Heading /> */}
+     <h1 id="heading">hii</h1>
+    <h2>hiiiiiiiiiii</h2> </div>
+  
+)
  const root = ReactDOM.createRoot(document.getElementById('root'));
- root.render(parent);
+ root.render(<Headingfunctional />);
+
